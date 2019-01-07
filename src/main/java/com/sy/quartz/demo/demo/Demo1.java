e1c2ff2b3efc0504eded5fc2554b9adb193ea9cf
wushiyan <shiyan_w@gzleihou.cn>
2019-01-07 18:27:32 +0800

    hahah

--- src/main/java/com/sy/quartz/demo/demo/Demo1.java
+++ src/main/java/com/sy/quartz/demo/demo/Demo1.java
@@ -6,6 +6,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.IOException;
+
 import static org.quartz.JobBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 import static org.quartz.CronScheduleBuilder.*;
@@ -13,29 +16,40 @@ import static org.quartz.CalendarIntervalScheduleBuilder.*;
 import static org.quartz.TriggerBuilder.*;
 import static org.quartz.DateBuilder.*;
 
-@RunWith(JUnit4.class)
 public class Demo1 {
 
 
     @Test
     public void test1() {
         try {
-            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            SchedulerFactory schedFact = new org.quartz.impl.StdSchedulerFactory();
 
+            Scheduler sched = schedFact.getScheduler();
 
-            JobDetail jd = newJob(HelloJob.class)
-                    .withIdentity("helloJob", "testGroup")
+            sched.start();
+
+            // define the job and tie it to our HelloJob class
+            JobDetail job = newJob(HelloJob.class)
+                    .withIdentity("myJob", "group1")
                     .build();
 
+            // Trigger the job to run now, and then every 40 seconds
             Trigger trigger = newTrigger()
-                    .withIdentity("helloTrigger", "testGroup")
+                    .withIdentity("myTrigger", "group1")
                     .startNow()
-                    .withSchedule(simpleSchedule().withIntervalInSeconds(10)
-                    .withRepeatCount(3))
+                    .withSchedule(simpleSchedule()
+                            .withIntervalInSeconds(40)
+                            .repeatForever())
                     .build();
-            scheduler.scheduleJob(jd, trigger);
-            scheduler.start();
+
+            // Tell quartz to schedule the job using our trigger
+            sched.scheduleJob(job, trigger);
         } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
+        try {
+            System.in.read();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
